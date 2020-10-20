f92a4ce4e5e7d1998ead8ced308af537d8b23356
Alexandr Somov <aleksandr.somov2011@gmail.com>
2020-10-20 20:59:49 +0300

    get data from a server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessageComponent } from './message/message.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { MessageComponent } from './message/message.component';
     HeroesComponent,
     DashboardComponent,
     HeroDetailComponent,
-    MessageComponent
+    MessageComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
